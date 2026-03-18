aa44e4a7d8894fd902ff1c7d13b770d3b34bb954
Ally Hamis RWEMERA <hamis.rwemera24snhu@keplercollege.ac.rw>
2026-03-18 15:54:50 +0200

    Add solution - CodeHub

---
+++ 8-kyu/are-you-playing-banjo/are-you-playing-banjo.js
@@ -0,0 +1,9 @@
+function areYouPlayingBanjo(name) {
+let firstLetter = name [0];
+  
+  if (firstLetter =="R" || firstLetter == "r") {
+    return name + " plays banjo";
+  } else { 
+    return name + " does not play banjo";
+  }
+  }
