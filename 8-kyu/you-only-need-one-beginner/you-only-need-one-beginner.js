9a49da191f067eb6eac3951133467c17c3c05055
Ally Hamis RWEMERA <hamis.rwemera24snhu@keplercollege.ac.rw>
2026-03-30 14:38:09 +0200

    Update solution - CodeHub

--- 8-kyu/you-only-need-one-beginner/you-only-need-one-beginner.js
+++ 8-kyu/you-only-need-one-beginner/you-only-need-one-beginner.js
@@ -1,1 +1,3 @@
-function check(a, x) {if(a.includes(x)){return true;} else {return false;}}
+//function check(a, x) {if(a.includes(x)){return true;} else {return false;}} 
+//Now am going to see if I can run it with only terrnary operators!
+function check(a, x) { return (a.includes(x))? true:false}
