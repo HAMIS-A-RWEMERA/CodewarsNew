058f90e1fb5200963ef446b1bb2b0aeec972a7d7
Ally Hamis RWEMERA <hamis.rwemera24snhu@keplercollege.ac.rw>
2026-04-04 23:37:58 +0200

    Add solution - CodeHub

---
+++ undefined/undefined/undefined.js
@@ -0,0 +1,11 @@
+function sumArray(array) {
+  if (!array || array.length <= 1) {
+    return 0;
+  }
+  const sorted = array.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 1; i < sorted.length - 1; i++) {
+    sum += sorted[i];
+  }
+  return sum;
+}
